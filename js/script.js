b26498f53d53c4c3d62ae9a5d4f14754f55ce263
Lamour123 <124379380+Lamour123@users.noreply.github.com>
2023-03-22 23:33:27 +0000

    More spelling errors.

--- js/script.js
+++ js/script.js
@@ -9,24 +9,24 @@
 function enterClicked() {
   // taxrate
   const TAXRATE = 0.2005
-  // get the hours worked from the user
+  // get how many hours that the user worked
   let hoursWorked= parseFloat(document.getElementById("hours-worked").value)
 
 	// get the hourly pay from the user
   let hourlyPay= parseFloat(document.getElementById("hourly-pay").value)
 
-  //caculate the pay  
+  //calculate the pay  
   let totalPay= hoursWorked*hourlyPay
-  //caculate the taxes
+  //calculate the taxes
   let taxes= totalPay*TAXRATE
-   //caculate the net income
+   //calculate the net income
   let netIncome= totalPay-taxes
 
 
   // display the user's pay
   
   document.getElementById('answer1').innerHTML = "Your pay is $" +netIncome.toFixed(2);
-  // display how much the gouvernment will pay
-  document.getElementById('answer2').innerHTML = "The gouvernment will take $"+taxes.toFixed(2) 
+  // display how much the government will pay
+  document.getElementById('answer2').innerHTML = "The government will take $"+taxes.toFixed(2) 
 }
 
